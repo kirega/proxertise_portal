8e7bd8ee1ca75a8e77ea1d3d2063b1d3daa2e065
kirega <kirega@mesozi.com>
2018-08-06 19:35:39 +0300

    login Component HTTP client ported and working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,30 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { HttpClientModule } from '@angular/common/http';
+import { NavigationService } from './services/navigation.service';
+import { AuthService } from './services/auth.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    NavigationService,
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { NavigationService } from '../services/navigation.service';
+
 
 @Component({
   selector: 'app-login',
@@ -9,15 +12,39 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  constructor( private fb: FormBuilder,) {}
+  error = false;
+
+  constructor( private fb: FormBuilder,
+               private auth: AuthService,
+               private nav:NavigationService
+              ) {}
 
   ngOnInit() {
+    localStorage.clear()
     this.createform()
+  }
+
+  login(formdata) {
+    // console.log(formdata);
+    this.auth.login(formdata)
+      .subscribe(
+        data => {
+          localStorage.setItem('token', data.key);
+          localStorage.setItem('user', data.user.username);
+          localStorage.setItem('id', data.user.id);
+          this.nav.goto_index();
+        },
+        err => {
+          this.nav.goto_login();
+          this.error = true;
+          console.log(err);
+        }
+      )
   }
   createform() {
     this.loginForm = this.fb.group({
-        email: ['',
-          [Validators.required, Validators.email]
+        username: ['',
+          [Validators.required]
         ],
         password: ['',
           [Validators.required]
