6f335e703b25e290302a84d38f706375c42c5dc6
kirega <kirega@mesozi.com>
2018-08-15 14:15:31 +0300

    playlist page added and routed

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from '../login/login.component';
+import { LoginComponent } from '../login/login.component';
 import { HomeComponent } from '../home/home.component';
 import { SignupComponent } from '../signup/signup.component';
 import { DashboardComponent } from '../dashboard/dashboard.component';
@@ -8,13 +8,16 @@ import { NavComponent } from '../dashboard/nav/nav.component';
 import { AdvertsComponent } from '../dashboard/adverts/adverts.component';
 import { AdvertFormComponent } from '../dashboard/adverts/advert-form/advert-form.component';
 import { AdvertsListComponent } from '../dashboard/adverts/adverts-list/adverts-list.component';
+import { PlaylistComponent } from '../dashboard/playlist/playlist.component';
+import { PlaylistListComponent } from '../dashboard/playlist/playlist-list/playlist-list.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/index', pathMatch: 'full'},
-  { path: 'index', component: HomeComponent},
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'index', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'dashboard',
+  {
+    path: 'dashboard',
     component: DashboardComponent,
     children: [
       {
@@ -27,6 +30,7 @@ const routes: Routes = [
           },
         ]
       },
+
       {
         path: 'adverts',
         component: AdvertsComponent,
@@ -41,12 +45,23 @@ const routes: Routes = [
           },
         ]
       },
+
+      {
+        path: 'playlists',
+        component: PlaylistComponent,
+        children: [
+          {
+            path: '',
+            component: PlaylistListComponent
+          }
+        ]
+      }
     ]
   }
 ];
 
 @NgModule({
-  imports : [ RouterModule.forRoot(routes)],
-  exports : [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
